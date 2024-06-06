377c842b470bc443b09a71d7d64eb0ab8732da32
NaroMori <118432431+SOLIDusr@users.noreply.github.com>
2024-06-06 20:09:45 +0300

    v1.2.0

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
 import binascii
+import hashlib
+import os
+import string
+import subprocess
+import sys
+import uuid
+import requests
 import tkinter as tk
 from tkinter import filedialog, messagebox
 from PIL import Image
-import hashlib 
-import subprocess
-import os
-import datetime
-import sys
-import requests
 
 
 class Picture:
@@ -18,19 +19,16 @@ class Picture:
         self.hexCode = binascii.hexlify(open(path, "rb").read()).decode("utf-8")
         self.wayCode = ""
 
-    
-    def get_center_pixel(self):
+    def getCenterPixel(self):
         return self.image.getpixel((self.width // 2, self.height // 2))
-    
-    def generate_light_code(self, way, passlength):
-        for i in range(way, way + passlength):
-            self.wayCode += self.hexCode[i]
+
+    def generateLightCode(self, way, passLength):
+        self.wayCode = self.hexCode[way:way + passLength]
         return self.wayCode
 
 class App:
-    
-    def __init__(self, mode, systemArg = None) -> None:
-        self.__version__ = "1.1.0-r"
+    def __init__(self, mode, systemArg=None) -> None:
+        self.version = "1.2.0-r"
         try:
             self.checkUpdates()
         except VersionMismatch:
@@ -38,227 +36,179 @@ class App:
         self.root = tk.Tk()
         self.uniqueCode = 0
         if mode == 1:
-            self.sysArg = systemArg
+            self.systemArg = systemArg
             if messagebox.askyesno("Warning", "Use the light encoding?"):
-                self.light_encoding(22, self.sysArg)
+                self.lightEncoding(22, self.systemArg)
             else:
-                self.heavy_encoding(22, self.sysArg)
+                self.heavyEncoding(22, self.systemArg)
 
     def checkUpdates(self):
-        repo_url = "https://api.github.com/repos/SOLIDusr/Bitsology/releases/latest"
+        repoUrl = "https://api.github.com/repos/SOLIDusr/Bitsology/releases/latest"
         try:
-            response = requests.get(repo_url)
+            response = requests.get(repoUrl)
             response.raise_for_status()
-            latest_version = response.json()["tag_name"]
-            if self.__version__ != latest_version:
-                raise VersionMismatch(f"Current version: {self.__version__}, Latest version: {latest_version}")
+            latestVersion = response.json()["tag_name"]
+            if self.version != latestVersion:
+                print(f"Current version: {self.version}, Latest version: {latestVersion}")
+                raise VersionMismatch(f"Current version: {self.version}, Latest version: {latestVersion}")
         except requests.RequestException as e:
             messagebox.showerror("Error", f"Failed to check for updates: {e}")
 
     def copyToClipboard(self, text):
         process = subprocess.Popen(['clip.exe'], stdin=subprocess.PIPE, text=True)
         process.communicate(input=text)
-        
-    
-    def shufflepassword(self, password, factor,passlen):
-        shadpass = hashlib.sha256(password.encode()).hexdigest()[:passlen]
-        newpass = ""
+
+    def shufflePassword(self, password, factor, passLen):
+        shadPass = hashlib.sha256(password.encode()).hexdigest()[:passLen]
+        newPass = ""
         specials = "!@#$%&="
-        for i in range(len(shadpass)):
-            if i in range(factor):
-                if i % 2 == 0:
-                    newpass += shadpass[i].upper()
-                else:
-                    newpass += shadpass[i]
+        for i in range(len(shadPass)):
+            if i < factor:
+                newPass += shadPass[i].upper() if i % 2 == 0 else shadPass[i]
             else:
-                if i % 3 == 0:
-                    newpass += specials[i % len(specials)]
-                else:
-                    newpass += shadpass[i]
-        return newpass
-    
+                newPass += specials[i % len(specials)] if i % 3 == 0 else shadPass[i]
+        return newPass
+
     def selectFile(self):
         filePath = filedialog.askopenfilename(
-        title="Select an Image",
-        filetypes=[("Image Files", "*.png;*.jpg;*.jpeg;*.bmp;*.gif")])
+            title="Select an Image",
+            filetypes=[("Image Files", "*.png;*.jpg;*.jpeg;*.bmp;*.gif")]
+        )
         if not filePath:
             messagebox.showerror("Error", "No file selected")
             return
         return filePath
-    
-    def calculateWayAndFactorPixel(self, center_pixel):
-        if isinstance(center_pixel, int):
-            return center_pixel, center_pixel
-        return sum(center_pixel), center_pixel[0]
-    
+
+    def calculateWayAndFactorPixel(self, centerPixel):
+        if isinstance(centerPixel, int):
+            return centerPixel, centerPixel
+        return sum(centerPixel), centerPixel[0]
+
     def guiInit(self):
         self.root.geometry("600x300")
         self.root.title("Bitsology")
 
-        passlengthLabel = tk.Label(self.root, text="Enter password length (3-36):")
-        passlengthEntry = tk.Entry(self.root, width=10)
-        passlengthLabel.pack(pady=10)
-        passlengthEntry.pack(pady=10)
+        passLengthLabel = tk.Label(self.root, text="Enter password length (3-36):")
+        passLengthEntry = tk.Entry(self.root, width=10)
+        passLengthLabel.pack(pady=10)
+        passLengthEntry.pack(pady=10)
 
-        def get_pass_length():
+        def getPassLength():
             try:
-                length = int(passlengthEntry.get())
-                if 3 <= length <= 36:
-                    return length
-                else:
-                    return 22
+                length = int(passLengthEntry.get())
+                return length if 3 <= length <= 36 else 22
             except ValueError:
                 return 22
-            
-        lightButton = tk.Button(self.root, text="Light Encoding", command=lambda: self.light_encoding(get_pass_length()))
+
+        lightButton = tk.Button(self.root, text="Light Encoding", command=lambda: self.lightEncoding(getPassLength()))
         lightButton.pack(pady=20)
-        heavyButton = tk.Button(self.root, text="Heavy Encoding", command=lambda: self.heavy_encoding(get_pass_length()))
+        heavyButton = tk.Button(self.root, text="Heavy Encoding", command=lambda: self.heavyEncoding(getPassLength()))
         heavyButton.pack(pady=20)
-        deleteButton = tk.Button(self.root, text="Delete custom encoding", command=lambda: self.delete_custom_encoding())
+        deleteButton = tk.Button(self.root, text="Delete custom encoding", command=lambda: self.deleteCustomEncoding())
         deleteButton.pack(pady=20)
         self.root.mainloop()
-    
-    def delete_custom_encoding(self):
-        filepath = os.path.expanduser("~/AppData/Roaming/Bitsology/custom_enc.png")
-        if os.path.exists(filepath):
-            os.remove(filepath)
+
+    def deleteCustomEncoding(self):
+        filePath = os.path.expanduser("~/AppData/Roaming/Bitsology/custom_enc.png")
+        if os.path.exists(filePath):
+            os.remove(filePath)
             messagebox.showinfo("Success", "Custom encoding file deleted")
         else:
             messagebox.showerror("Error", "Custom encoding file not found")
 
-    def light_encoding(self, passlength, externalFile = None):
-        if externalFile:
-            filePath = externalFile
-        else:
-            filePath = self.selectFile()
-            if not filePath:
-                return
+    def lightEncoding(self, passLength, externalFile=None):
+        filePath = externalFile if externalFile else self.selectFile()
+        if not filePath:
+            return
         picture = Picture(filePath)
-        centerPixel = picture.get_center_pixel()
-        way, factorpixel = self.calculateWayAndFactorPixel(centerPixel)
-        rawpassword = picture.generate_light_code(way=way, passlength=passlength)
-        if factorpixel % 100 > passlength // 3 * 2:
-            factor = passlength // 3 * 2
-        elif factorpixel % 100 > passlength // 3:
-            factor = passlength // 3
-        else:
-            factor = passlength // 2
-        
-        passwd = self.shufflepassword(password=rawpassword, factor=factor, passlen=passlength)
-        self.copyToClipboard(passwd)
-        messagebox.showinfo("Password copied to clipboard", f"Your password is: {passwd}")
-    
-    def text_to_image(self, text, img_size=(100, 100)):
-        text_bytes = hashlib.sha256(text.encode()).digest()
-        image = Image.new('RGB', img_size)
+        centerPixel = picture.getCenterPixel()
+        way, factorPixel = self.calculateWayAndFactorPixel(centerPixel)
+        rawPassword = picture.generateLightCode(way=way, passLength=passLength)
+        factor = passLength // 3 * 2 if factorPixel % 100 > passLength // 3 * 2 else passLength // 3 if factorPixel % 100 > passLength // 3 else passLength // 2
+        password = self.shufflePassword(password=rawPassword, factor=factor, passLen=passLength)
+        self.copyToClipboard(password)
+        messagebox.showinfo("Password copied to clipboard", f"Your password is: {password}")
+
+    def textToImage(self, text, imgSize=(100, 100)):
+        textBytes = hashlib.sha256(text.encode()).digest()
+        image = Image.new('RGB', imgSize)
         pixels = image.load()
         index = 0
-        for i in range(img_size[0]):
-            for j in range(img_size[1]):
-                if index < len(text_bytes) - 2:
-                    pixels[i, j] = (text_bytes[index], text_bytes[index + 1], text_bytes[index + 2])
+        for i in range(imgSize[0]):
+            for j in range(imgSize[1]):
+                if index < len(textBytes) - 2:
+                    pixels[i, j] = (textBytes[index], textBytes[index + 1], textBytes[index + 2])
                     index += 3
                 else:
                     index = 0
-        
         return image
-        
+
     def getCEncPars(self) -> str:
-        string = ""
-        string += datetime.datetime.now().strftime("%d%m%Y%H%M%S")
-        string += os.getlogin()
-        return string
-    
-    def generate_custom_encoding(self, CEnc):
-        parts = [CEnc[i:i+len(CEnc)//16] for i in range(0, len(CEnc), len(CEnc)//16)]
-        sums = []
-        for part in parts:
-            sum = 0
-            for ch in part:
-                if ch in "0123456789":
-                    sum += int(ch)
-            sums.append(sum)
-        sums = [str(sum) for sum in sums]
+        return uuid.uuid1().hex
+
+    def generateCustomEncoding(self, cEnc):
+        parts = [cEnc[i:i + len(cEnc) // 16] for i in range(0, len(cEnc), len(cEnc) // 16)]
+        sums = [str(sum(int(ch) for ch in part if ch in "0123456789")) for part in parts]
         return "".join(sums)
-        
-    def encode(self, rawpass, cenc):
-        newpass = list(rawpass)
-        print(cenc)
-        for i in cenc:
-            if i == "1":
-                for q in range(len(newpass)):
-                    if q % 2 == 0:
-                        newpass[q] = newpass[q].upper()
-                    else:
-                        newpass[q] = newpass[q].lower()
-            elif i == "2":
-                newpass = newpass[::-1]
-            elif i == "3":
-                for q in range(len(newpass)):
-                    if newpass[q].isdigit():
-                        newpass[q] = str(9 - int(newpass[q]))
-            elif i == "4":
-                newpass = newpass[::-1]
-            elif i == "5":
-                newpass = newpass[:len(newpass)//2] + newpass[len(newpass)//2:][::-1]
-            elif i == "6":
-                newpass = newpass[:len(newpass)//2] + newpass[len(newpass)//2:][::-1]
-            elif i == "7":
-                newpass = newpass[:len(newpass)//2][::-1] + newpass[len(newpass)//2:]
-            elif i == "8":
-                newpass = newpass[::-1]
-            elif i == "9":
-                newpass[-1] = newpass[-2]
-            elif i == "0":
-                newpass[0] = newpass[-1]
+
+    def encode(self, rawPass, cEnc):
+        if len(cEnc) < len(rawPass):
+            cEnc = (cEnc * (len(rawPass) // len(cEnc) + 1))[:len(rawPass)]
+
+        hashedKey = hashlib.sha256(cEnc.encode()).digest()
+        allowedChars = string.ascii_letters + string.digits + "!@#$%&="
+        allowedCharsLen = len(allowedChars)
+
+        encodedChars = []
+        for i in range(len(rawPass)):
+            xorResult = ord(rawPass[i]) ^ hashedKey[i % len(hashedKey)]
+            encodedChar = allowedChars[xorResult % allowedCharsLen]
+            encodedChars.append(encodedChar)
+
+        encodedText = ''.join(encodedChars)
         specials = "!@#$%&="
-        for i in range(len(newpass)):
-            if newpass[i].isdigit() and i % 3 == 0:
-                newpass[i] = specials[i % len(specials)]
-        return "".join(newpass)  
-        
-        
-    def heavy_encoding(self, passlength, externalFile = None):
+        passList = list(encodedText)
+        for i in range(len(passList)):
+            if passList[i].isdigit() and i % 5 == 0:
+                passList[i] = specials[i % len(specials)]
+            elif passList[i].isalpha() and i % 4 == 0:
+                passList[i] = passList[i].upper()
+            elif passList[i].isalpha() and i % 6 == 0:
+                passList[i] = specials[(i + 5) % len(specials)]
+            if passList[i].isalpha() and i > len(passList) // 2 and i % 3 == 0:
+                passList[i] = "0123456789"[i % 10]
+
+        return "".join(passList)
+
+    def heavyEncoding(self, passLength, externalFile=None):
         text = self.getCEncPars()
-        if externalFile:
-            pathToPicture = externalFile
-        else:
-            pathToPicture = self.selectFile()
-            if not pathToPicture:
-                return
-        if not pathToPicture:
+        print(text)
+        filePath = externalFile if externalFile else self.selectFile()
+        if not filePath:
             return
-        picture = Picture(pathToPicture)
-        way, _ = self.calculateWayAndFactorPixel(picture.get_center_pixel())
-        rawpass = picture.generate_light_code(way=way, passlength=passlength)
-        dirpath = os.path.expanduser("~/AppData/Roaming/Bitsology/")
-        filepath = os.path.expanduser("~/AppData/Roaming/Bitsology/custom_enc.png")
-        if not os.path.exists(dirpath):
-            os.makedirs(dirpath)
-        if not os.path.exists(filepath):
+        picture = Picture(filePath)
+        way, _ = self.calculateWayAndFactorPixel(picture.getCenterPixel())
+        rawPass = picture.generateLightCode(way=way, passLength=passLength)
+        dirPath = os.path.expanduser("~/AppData/Roaming/Bitsology/")
+        filePath = os.path.expanduser("~/AppData/Roaming/Bitsology/custom_enc.png")
+        if not os.path.exists(dirPath):
+            os.makedirs(dirPath)
+        if not os.path.exists(filePath):
             if not messagebox.askokcancel("Warning", "No custom encoding file found, do you want to create a new one?"):
                 return
-            if isinstance(text, str):
-                image = self.text_to_image(text)
-            else:
-                print("Error: Expected a string from getCEncPars, got:", type(text))
-            image.save(filepath)
-            
-        image = binascii.hexlify(open(filepath, "rb").read()).decode("utf-8")
-        encoding = self.generate_custom_encoding(image)
-        print(rawpass)
-        mypass = self.encode(rawpass=rawpass, cenc=encoding)
-        print(mypass)
-        self.copyToClipboard(mypass)
-        messagebox.showinfo("Password copied to clipboard", f"Your password is: {mypass}")
+            image = self.textToImage(text)
+            image.save(filePath)
 
+        imageHex = binascii.hexlify(open(filePath, "rb").read()).decode("utf-8")
+        encoding = self.generateCustomEncoding(imageHex)
+        password = self.encode(rawPass=rawPass, cEnc=encoding)
+        self.copyToClipboard(password)
+        messagebox.showinfo("Password copied to clipboard", f"Your password is: {password}")
 
 class VersionMismatch(Exception):
     def __init__(self, message="Version mismatch error"):
         self.message = message
         super().__init__(self.message)
-
-
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
